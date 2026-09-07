139db00dbc79c74d9eaf733aec549181a8cdd841
Yash Negi <yashnegi36442@gmail.com>
2026-09-08 03:30:57 +0530

    Time: 21 ms (7.44%) | Memory: 47.4 MB (9.14%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+        for(int i=0; i<t.length(); i++){
+            char ch = t.charAt(i);
+            if(map.containsKey(ch)){
+                int count = map.get(ch);
+                if (count==1){
+                    map.remove(ch);
+                }
+                else {
+                    count--;
+                    map.remove(ch);
+                    map.put(ch, count);
+                }
+            }
+        }
+        return map.isEmpty()? true : false;
+
+        // Or simply we can use the sort function:
+        // char ch1[] = s.toCharArray();
+        // char ch2[] = t.toCharArray();
+        // Arrays.sort(ch1);
+        // Arrays.sort(ch2);
+        // return Arrays.equals(ch1, ch2);
+    }
+}
