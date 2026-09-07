dc9552f53dd45b5ee859844207ceb963db99fed7
Yash Negi <yashnegi36442@gmail.com>
2026-09-08 03:35:01 +0530

    Time: 2 ms (99.64%) | Memory: 44.8 MB (52.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,27 +1,43 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()){
-            return false;
+        int[] freq = new int[26];
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
         }
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        for(char ch : t.toCharArray()){
+            freq[ch-'a']--;
         }
-        for(int i=0; i<t.length(); i++){
-            char ch = t.charAt(i);
-            if(map.containsKey(ch)){
-                int count = map.get(ch);
-                if (count==1){
-                    map.remove(ch);
-                }
-                else {
-                    count--;
-                    map.remove(ch);
-                    map.put(ch, count);
-                }
+        for (int count : freq){
+            if (count != 0){
+                return false;
             }
         }
-        return map.isEmpty()? true : false;
+        return true;
+
+        // if (s.length() != t.length()){
+        //     return false;
+        // }
+        // Map<Character, Integer> map = new HashMap<>();
+        // for(int i=0; i<s.length(); i++){
+        //     map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        // }
+        // for(int i=0; i<t.length(); i++){
+        //     char ch = t.charAt(i);
+        //     if(map.containsKey(ch)){
+        //         int count = map.get(ch);
+        //         if (count==1){
+        //             map.remove(ch);
+        //         }
+        //         else {
+        //             count--;
+        //             map.remove(ch);
+        //             map.put(ch, count);
+        //         }
+        //     }
+        // }
+        // return map.isEmpty()? true : false;
+
+        // --------------------------------------------------------------------------------
 
         // Or simply we can use the sort function:
         // char ch1[] = s.toCharArray();
